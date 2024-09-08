94d2921e7292f63007f2d3d1cfea970881210de7
ducthang001 <ducthang123306@gmail.com>
2024-09-08 09:57:03 +0700

    add inventory model

--- src/services/product.service.xxx.js
+++ src/services/product.service.xxx.js
@@ -7,6 +7,7 @@ const {
   clothing,
   furniture,
 } = require("../models/product.model");
+const { insertInventory } = require("../models/repository/inventory.repo");
 const {
   findAllDraftsForShop,
   publishProductByShop,
@@ -117,7 +118,17 @@ class Product {
 
   // create new product
   async createProduct(product_id) {
-    return await product.create({ ...this, _id: product_id });
+    const newProduct = await product.create({ ...this, _id: product_id });
+    if(newProduct) {
+      // add product_stock in inventory collection
+      await insertInventory({
+        productId: newProduct._id,
+        shopId: this.product_shop,
+        stock: this.product_quantity
+      })
+    }
+
+    return newProduct
   }
 
   // update Product 
